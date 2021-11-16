be5df3ffe8a1635f8f32779fa456e56c785fc3ed
Otmane Aatik <aaotmane@gmail.com>
2021-11-16 18:35:35 -0500

    hrader footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Card, CardImg, CardBody, CardText, CardTitle} from 'reactstrap';
 
-class CampsiteInfo extends Component {
     
-    renderCampsite(campsite){
+function RenderCampsite({campsite}){
         return(
             <div className="col-md-5 m-1">
                 <Card>
@@ -14,9 +13,9 @@ class CampsiteInfo extends Component {
                     </CardBody>
                 </Card>
             </div>
-        )
+        );
     } 
-    renderComments(comments){
+function RenderComments({comments}){
         if(comments){
             return(
                 <div className="col-md-5 m-1">
@@ -33,18 +32,18 @@ class CampsiteInfo extends Component {
         return <div/>
 
     }
-    render() {
-        if(this.props.campsite){
+function CampsiteInfo(props) {
+        if(props.campsite){
             return(
             <div className="container">
               <div className="row">
-                   {this.renderCampsite(this.props.campsite)}
-                   {this.renderComments(this.props.campsite.comments)}
+                   <RenderCampsite campsite={props.campsite} />
+                   <RenderComments comments={props.campsite.comments} />
               </div>
             </div>)
         }
         return <div/>
     }
-}
+
 
 export default CampsiteInfo;
