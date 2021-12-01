8c8f9fdf6de07283331b4c651c5fd17aa8774794
Otmane Aatik <aaotmane@gmail.com>
2021-12-01 14:09:14 -0500

    React redux form revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 // import Reducers to combine them 
 import { Campsites } from './campsites';
 import { Comments } from './comments';
@@ -12,7 +14,10 @@ export const ConfigureStore = () => {
         campsites: Campsites,
         comments: Comments,
         partners: Partners, 
-        promotions: Promotions
+        promotions: Promotions,
+        ...createForms({
+            feedBackForm: InitialFeedback
+        })
     }) , applyMiddleware(thunk, logger));
     return store
 }
