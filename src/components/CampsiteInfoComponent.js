87d721b14746aaed32c039a8c34d48f71c2f6617
Otmane Aatik <aaotmane@gmail.com>
2021-12-04 15:03:01 -0500

    react transition

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -24,7 +24,7 @@ class CommentForm extends Component {
   }
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text)
   }
     render(){
         return(
@@ -107,7 +107,7 @@ function RenderCampsite({campsite}){
             </div>
         );
     } 
-function RenderComments({comments, addComment, campsiteId}){
+function RenderComments({comments, postComment, campsiteId}){
         if(comments){
             return(
                 <div className="col-md-5 m-1">
@@ -118,7 +118,7 @@ function RenderComments({comments, addComment, campsiteId}){
                         {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                         </p>
                     </div>)})}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+                    <CommentForm campsiteId={campsiteId} postComment={postComment}/>
                 </div>
             )
         }
@@ -161,7 +161,7 @@ function CampsiteInfo(props) {
                    <RenderCampsite campsite={props.campsite} />
                    <RenderComments 
                    comments={props.comments} 
-                   addComment={props.addComment}
+                   postComment={props.postComment}
                    campsiteId={props.campsite.id}
                    />
               </div>
